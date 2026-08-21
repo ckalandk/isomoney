6673966b0c79dfb02a7326e510bd8c408c9bea9a
ckalandk <ckalandk.main@gmail.com>
2026-08-21 02:26:45 +0100

    chore: bump version to 1.0.1 and update development status to production/stable

--- src/pycents/__init__.py
+++ src/pycents/__init__.py
@@ -9,7 +9,7 @@ from .exceptions import (
 from .money import Money, UnroundedMoney
 from .rounding import RoundingMode
 
-__version__ = "1.0.0"
+__version__ = "1.0.1"
 
 __all__ = [
     # Core domain modeles
